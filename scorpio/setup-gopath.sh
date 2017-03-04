46f15dc9afe7b363f066424afd0c7cb031e97d1b
yangchuanhu <yangchuanhu@xiaomi.com>
2017-03-04 11:00:47 +0800

    修改setup-gopath脚本

--- scorpio/setup-gopath.sh
+++ scorpio/setup-gopath.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-if [ -d $(dirname $DIR)/stardust.go ]; then
-    export GOPATH="$DIR:$(dirname $DIR)/stardust.go"
-else
-    export GOPATH="$DIR:$(dirname $DIR)/stardust-go"
-fi
+export GOPATH="$DIR:$(dirname $DIR)/stardust-go"
